aeba9fcbc610284fe1f6632c4df089b15c6acc08
tusharkale77 <tushar.k@syborgtech.com>
2025-04-10 11:59:21 +0530

    Add figma design

--- src/Pages/CreateFan1.jsx
+++ src/Pages/CreateFan1.jsx
@@ -1,213 +1,297 @@
-import React from "react";
 import {
   Box,
-  TextField,
   Button,
+  Container,
+  Divider,
+  Grid,
+  Paper,
+  Stack,
+  TextField,
   Typography,
-  Tabs,
-  Tab,
-  Input,
 } from "@mui/material";
-import { Divider } from "@mui/material";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import AddIcon from "@mui/icons-material/Add";
 
-const CreateFan1 = () => {
+const CreateFan = () => {
   const navigate = useNavigate();
 
-  return (
-    <>
-      <Box sx={{ pl: 9, pr: 9, mt: 1 }}>
-        <Box>
-          {/* <Typography variant="h6" sx={{ fontWeight: "bold" }}>
-            Create Pump
-          </Typography> */}
-          <h2> Create Fan</h2>
-        </Box>
+  // State for form fields
+  const [formData, setFormData] = useState({
+    make: "",
+    model: "",
+    exhaustFanRpm: "",
+    exhaust:"",
+    supply:"",
+    size: "",
+    voltsPhase: "",
+    supplyFanRpm: "",
+    rpm: "",
+  });
 
-        <Box
-          component="form"
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = () => {
+  navigate("/project2");
+  };
+
+  return (
+    <Box
+               sx={{
+                 bgcolor: "#f2f4f5",
+                 minHeight: "100vh",
+                 py: 4,
+                 pl: "70px",
+                 pt: "30px",
+                 pr: "24px",
+                 boxSizing: "border-box",
+                 overflow: "hidden",
+               }}
+             >
+        
+        <Typography
+             variant="h5" component="h1" fontWeight="bold" sx={{ mb: 4 }}
+           >
+             Create Fan
+           </Typography>
+
+        <Paper
+          elevation={4}
           sx={{
-            display: "flex",
-            flexDirection: "column",
-            gap: 2,
-            maxWidth: 1100,
-            margin: "auto",
-            padding: 3,
-            boxShadow: 3,
-            borderRadius: 2,
+            p: 4,
+            borderRadius: "10px",
+            mb: 4,
           }}
         >
-          <Typography variant="h6" sx={{ fontWeight: 600 }}>
+          <Typography
+            variant="h5"
+            fontWeight="bold"
+            sx={{ mb: 3, fontFamily: "Poppins" }}
+          >
             Fan
           </Typography>
-          <Box
-            sx={{
-              display: "grid",
-              gridTemplateColumns: "repeat(2, 1fr)", // 2 columns
-              gap: 2, // Spacing between items
-            }}
+
+          <Grid container spacing={4}>
+            <Grid item xs={12} md={6}>
+              <Stack direction="row" spacing={2} alignItems="center">
+                <Typography
+                  sx={{
+                    width: 100,
+                    fontFamily: "Poppins",
+                    fontWeight: 500,
+                  }}
+                >
+                  Make :
+                </Typography>
+                <TextField
+                  fullWidth
+                  size="small"
+                  name="make"
+                  value={formData.make}
+                  onChange={handleChange}
+                  variant="outlined"
+                />
+              </Stack>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Stack direction="row" spacing={2} alignItems="center">
+                <Typography
+                  sx={{
+                    width: 100,
+                    fontFamily: "Poppins",
+                    fontWeight: 500,
+                  }}
+                >
+                  Model :
+                </Typography>
+                <TextField
+                  fullWidth
+                  size="small"
+                  name="model"
+                  value={formData.model}
+                  onChange={handleChange}
+                  variant="outlined"
+                />
+              </Stack>
+            </Grid>
+
+            <Grid item xs={12} md={6}>
+              <Stack direction="row" spacing={2} alignItems="center">
+                <Typography
+                  sx={{
+                    width: 100,
+                    fontFamily: "Poppins",
+                    fontWeight: 500,
+                  }}
+                >
+                  Supply Fan R.P.M :
+                </Typography>
+                <TextField
+                  fullWidth
+                  size="small"
+                  name="supplyFanRpm"
+                  value={formData.supplyFanRpm}
+                  onChange={handleChange}
+                  variant="outlined"
+                />
+              </Stack>
+            </Grid>
+
+            <Grid item xs={12} md={6}>
+              <Stack direction="row" spacing={2} alignItems="center">
+                <Typography
+                  sx={{
+                    width: 100,
+                    fontFamily: "Poppins",
+                    fontWeight: 500,
+                  }}
+                >
+                  Exhaust Fan R.P.M :
+                </Typography>
+                <TextField
+                  fullWidth
+                  size="small"
+                  name="exhaustFanRpm"
+                  value={formData.exhaustFanRpm}
+                  onChange={handleChange}
+                  variant="outlined"
+                />
+              </Stack>
+            </Grid>
+
+          </Grid>
+
+          <Divider
+            sx={{ my: 3}}
+          />
+
+          <Typography
+            variant="h5"
+            fontWeight="bold"
+            sx={{ mb: 3, fontFamily: "Poppins" }}
           >
-            <Box display="flex" alignItems="center">
-              <Typography sx={{ minWidth: "110px" }}>Make :</Typography>
-              <Input
-                fullWidth
-                name="make"
-                sx={{
-                  border: "1px solid #ccc",
-                  padding: "5px",
-                  borderRadius: "4px",
-                }}
-              />
-            </Box>
-
-            <Box display="flex" alignItems="center">
-              <Typography sx={{ minWidth: "110px" }}>Model :</Typography>
-              <Input
-                fullWidth
-                name="model"
-                sx={{
-                  border: "1px solid #ccc",
-                  padding: "5px",
-                  borderRadius: "4px",
-                }}
-              />
-            </Box>
-          </Box>
-          <Divider />
-
-          <Typography variant="h6" sx={{ fontWeight: 600 }}>
             Motor
           </Typography>
-          <Box
-            sx={{
-              display: "grid",
-              gridTemplateColumns: "repeat(1, 1fr)", // 2 columns
-              gap: 2, // Spacing between items
-            }}
+
+          <Typography
+            variant="h6"
+            fontWeight="semibold"
+            sx={{ mb: 2, fontFamily: "Poppins" }}
           >
-            <Box display="flex" alignItems="center">
-              <Typography sx={{ minWidth: "110px" }}>Size :</Typography>
-              <Typography sx={{ minWidth: "60px" }}>Supply :</Typography>
-              <Input
-                name="supply"
-                fullWidth
-                sx={{
-                  border: "1px solid #ccc",
-                  padding: "5px",
-                  borderRadius: "4px",
-                  marginRight: "8px",
-                }}
-              />
-              <Typography sx={{ minWidth: "60px" }}>Exhaust :</Typography>
-              <Input
-                name="exhaust"
-                fullWidth
-                sx={{
-                  border: "1px solid #ccc",
-                  padding: "5px",
-                  borderRadius: "4px",
-                }}
-              />
-            </Box>
+            Size
+          </Typography>
 
-            <Box display="flex" alignItems="center">
-              <Typography sx={{ minWidth: "110px" }}>Volts/Phase :</Typography>
-              <Input
-                fullWidth
-                name="volts"
-                sx={{
-                  border: "1px solid #ccc",
-                  padding: "5px",
-                  borderRadius: "4px",
-                }}
-              />
-            </Box>
+          <Grid container spacing={4}>
+          
+            <Grid item xs={12} md={6}>
+              <Stack direction="row" spacing={2} alignItems="center">
+                <Typography
+                  sx={{
+                    width: 100,
+                    fontFamily: "Poppins",
+                    fontWeight: 500,
+                  }}
+                >
+                  Supply :
+                </Typography>
+                <TextField
+                  fullWidth
+                  size="small"
+                  name="supply"
+                  value={formData.supply}
+                  onChange={handleChange}
+                  variant="outlined"
+                />
+              </Stack>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Stack direction="row" spacing={2} alignItems="center">
+                <Typography
+                  sx={{
+                    width: 100,
+                    fontFamily: "Poppins",
+                    fontWeight: 500,
+                  }}
+                >
+                  Exhaust :
+                </Typography>
+                <TextField
+                  fullWidth
+                  size="small"
+                  name="exhaust"
+                  value={formData.exhaust}
+                  onChange={handleChange}
+                  variant="outlined"
+                />
+              </Stack>
+            </Grid>
 
-            {/* <Box display="flex" alignItems="center">
-              <Typography sx={{ minWidth: "110px" }}>
-                {" "}
-                Supply Amperage :
-              </Typography>
-              <Input
-                fullWidth
-                name="amperage"
-                sx={{
-                  border: "1px solid #ccc",
-                  padding: "5px",
-                  borderRadius: "4px",
-                }}
-              />
-            </Box>
+            <Grid item xs={12} md={6}>
+              <Stack direction="row" spacing={2} alignItems="center">
+                <Typography
+                  sx={{
+                    width: 100,
+                    fontFamily: "Poppins",
+                    fontWeight: 500,
+                  }}
+                >
+                  Volts/
+                  Phase :
+                </Typography>
+                <TextField
+                  fullWidth
+                  size="small"
+                  name="voltsPhase"
+                  value={formData.voltsPhase}
+                  onChange={handleChange}
+                  variant="outlined"
+                />
+              </Stack>
+            </Grid>
+          </Grid>
+        </Paper>
 
-            <Box display="flex" alignItems="center">
-              <Typography sx={{ minWidth: "110px" }}>
-                Exhaust Amperage :
-              </Typography>
-              <Input
-                fullWidth
-                name="rpm"
-                sx={{
-                  border: "1px solid #ccc",
-                  padding: "5px",
-                  borderRadius: "4px",
-                }}
-              />
-            </Box>
-            <Box display="flex" alignItems="center">
-              <Typography sx={{ minWidth: "110px" }}>
-                {" "}
-                Supply R.P.M :
-              </Typography>
-              <Input
-                fullWidth
-                name="rpm"
-                sx={{
-                  border: "1px solid #ccc",
-                  padding: "5px",
-                  borderRadius: "4px",
-                }}
-              />
-            </Box>
-            <Box display="flex" alignItems="center">
-              <Typography sx={{ minWidth: "110px" }}>
-                {" "}
-                Exhaust R.P.M :
-              </Typography>
-              <Input
-                fullWidth
-                name="rpm"
-                sx={{
-                  border: "1px solid #ccc",
-                  padding: "5px",
-                  borderRadius: "4px",
-                }}
-              />
-            </Box> */}
-          </Box>
-          <Box display="flex" justifyContent="space-between">
-            <Box>
-              <Button variant="outlined" startIcon={<AddIcon />}>
-                Add Parameters
-              </Button>
-            </Box>
-          </Box>
-        </Box>
-        <Box display="flex" justifyContent="flex-end" sx={{ mt: 2 }}>
+        <Box sx={{ display: "flex", justifyContent: "flex-end", gap: 2 }}>
           <Button
             variant="outlined"
             onClick={() => navigate("/createFanCreation")}
-            sx={{ mr: 2 }}
+            sx={{
+              borderRadius: "10px",
+              px: 3,
+              bgcolor: "#f2f4f5",
+              fontFamily: "Poppins",
+              fontWeight: 500,
+              border: "1px solid black",
+              color: "black",
+            }}
           >
             Back
           </Button>
-          <Button variant="contained" onClick={() => navigate("/projects")}>
+          <Button
+            variant="contained"
+            onClick={handleSubmit}
+            sx={{
+              borderRadius: "10px",
+              px: 3,
+              bgcolor: "#99caff",
+              color: "black",
+              fontFamily: "Poppins",
+              fontWeight: 500,
+              "&:hover": {
+                bgcolor: "#7bb8ff",
+              },
+            }}
+          >
             Submit
           </Button>
         </Box>
-      </Box>
-    </>
+    </Box>
   );
 };
 
-export default CreateFan1;
+export default CreateFan;
